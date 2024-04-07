f19f9acb8886da3ec8f470466231d8fb80b4203a
SniffSpoof <52812804+SniffSpoof@users.noreply.github.com>
2024-04-07 18:22:45 +0300

    Update fuzzy_logic.py

--- fuzzy_logic.py
+++ fuzzy_logic.py
@@ -1,144 +1,91 @@
-'''
-This is the main library file. 
+"""
+This is the main library file.
 The fuzzy set class and quantifiers are described here.
-'''
+"""
+from typing import List, Union, Any
 
 import fuzzy_funcs
-import math as m
+import fuzzy_kvant
 import matplotlib.pyplot as plt
 
+
 class fuzzy_set(object):
-	def __init__(self, type = "Empty", U1 = None, U2 = None, a = None, b = None, c = None, epsilon = 1, U = None):
-		#init of params
-		#U1 and U2 its definition set bounds
-		if((U1 != None) and (U2 != None)):
-			self.U = fuzzy_funcs.generate_U(U1, U2, epsilon)
-		else:
-			self.U = U
-		self.a = a
-		self.b = b
-		self.c = c
-		self.type = type
-		
-		#init func
-		if (type == "S"):
-			self.f = fuzzy_funcs.S_func(self.U, self.a, self.b, self.c)
-		elif (type == "t"):
-			self.f = fuzzy_funcs.t_func(self.U, self.a, self.b, self.c)
-		elif (type == "y"):
-			self.f = fuzzy_funcs.y_func(self.U, self.a, self.b)
-		elif (type == "L"):
-			self.f = fuzzy_funcs.L_func(self.U, self.a, self.b)
-		elif (type == "PI"):
-			self.f = fuzzy_funcs.PI_func(self.U, self.a, self.b, self.c)
-		elif (type == "Empty"):
-			self.f = []
-		else:
-			self.f = []
-			for i in self.U:
-				try:
-					v = ((i+b)/(a))**c
-				except:					#if zero zero div
-					v = ((i+b)/(a+0.00001))**c
-				self.f.append(1/(1+v))
-			
-	def init_params(self):
-		self.height = max(self.f)
-		self.sup = set(self.f)
-		
-	def alpha_cut(self, alpha):
-		#go through the set of values, if less than alpha - 0, if greater or equal - value at point
-		self.f_alpha = []
-		for i in self.f:
-			if(i >= alpha):
-				self.f_alpha.append(i)
-			else:
-				self.f_alpha.append(0)
-			
-	def plot(self):
-		graph = plt.plot(self.U, self.f)
-		grid1 = plt.grid(True)
-	
-	def plot_scatter(self):
-		graph = plt.scatter(self.U, self.f, marker='o')
-		grid1 = plt.grid(True)
-		
-		
-	def kvant(self, type, f2 = None):
-		if (type == "not"):
-			self.f = not_a(self.f)
-		elif (type == "very"):
-			self.f = very_a(self.f)
-			print(self.f)
-		elif (type == "MOL"):
-			self.f = MOL_a(self.f)
-		elif (type == "or"):
-			b = f2.f
-			marker = 0 #if sets have different sizes, then comparisons will occur on a smaller
-			self.f, marker = a_or_b(self.f, b)
-			if(marker == 1):
-				self.U = f2.U
-		elif (type == "and"):
-			b = f2.f
-			marker = 0
-			self.f, marker = a_and_b(self.f, b)
-			if(marker == 1):
-				self.U = f2.U
-		else:
-			print("None")
-			
-def show():
-	plt.show()
-	
-def not_a(U):
-	a = []
-	for el in U:
-		a.append(1 - el)
-	return a
-	
-def very_a(U):
-	a = []
-	for el in U:
-		a.append(el**2)
-	return a
-	
-def MOL_a(U):
-	#more or less
-	a = []
-	for el in U:
-		a.append(m.sqrt(el))
-	return a
-	
-def a_or_b(U1, U2):
-	a = []
-	i = 0
-	marker = 0
-	if (len(U1) > len(U2)):
-		c = U1
-		U1 = U2
-		U2 = c
-		marker = 1
-	for el in U1:
-		try:
-			a.append(min(el, U2[i]))
-		except:					#if went beyond the function definition
-			a.append(0)
-		i+=1
-	return a, marker
-	
-def a_and_b(U1, U2):
-	a = []
-	i = 0
-	marker = 0
-	if (len(U1) > len(U2)):
-		c = U1
-		U1 = U2
-		U2 = c
-		marker = 1
-	for el in U1:
-		try:
-			a.append(min(el, U2[i]))
-		except:
-			a.append(0)
-		i+=1
-	return a, marker
+    def __init__(self, func_type="Empty", l=None, r=None, a=None, b=None, c=None, epsilon=1, u=None):
+        if (l is not None) and (r is not None):
+            self.universum = fuzzy_funcs.Universum(l, r, epsilon)
+        else:
+            self.universum = u
+
+        self.a = a
+        self.b = b
+        self.c = c
+        self.type = func_type
+
+        if func_type == "S":
+            self.f = fuzzy_funcs.s_func(self.universum.u, self.a, self.b, self.c)
+        elif func_type == "t":
+            self.f = fuzzy_funcs.t_func(self.universum.u, self.a, self.b, self.c)
+        elif func_type == "y":
+            self.f = fuzzy_funcs.y_func(self.universum.u, self.a, self.b)
+        elif func_type == "L":
+            self.f = fuzzy_funcs.l_func(self.universum.u, self.a, self.b)
+        elif func_type == "PI":
+            self.f = fuzzy_funcs.pi_func(self.universum.u, self.a, self.b, self.c)
+        elif func_type == "Empty":
+            self.f = []
+        else:
+            self.f = []
+            for i in self.u:
+                if a != 0:
+                    v = ((i + b) / a) ** c
+                else:
+                    v = ((i + b) / (a + 0.00001)) ** c
+                self.f.append(1 / (1 + v))
+
+    def init_params(self) -> int:
+        return max(self.f)
+
+    def get_sup(self) -> set:
+        return set(self.f)
+
+    def alpha_cut(self, alpha) -> list[Union[Union[float, int], Any]]:
+        # go through the set of values, if less than alpha - 0, if greater or equal - value at point
+        f_alpha = []
+        for i in self.f:
+            if i >= alpha:
+                f_alpha.append(i)
+            else:
+                f_alpha.append(0)
+        return f_alpha
+
+    def plot(self) -> None:
+        graph = plt.plot(self.universum.u, self.f)
+        plt.grid(True)
+
+    def plot_scatter(self) -> None:
+        graph = plt.scatter(self.universum.u, self.f, marker='o')
+        plt.grid(True)
+
+    def kvant(self, kvant_type, f2=None) -> None:
+        if kvant_type == "not":
+            self.f= fuzzy_kvant.not_a(self.f)
+        elif kvant_type == "very":
+            self.f = fuzzy_kvant.very_a(self.f)
+        elif kvant_type == "MOL":
+            self.f = fuzzy_kvant.mol_a(self.f)
+        elif kvant_type == "or":
+            marker = 0  # if sets have different sizes, then comparisons will occur on a smaller
+            self.f, marker = fuzzy_kvant.a_or_b(self.f, f2.f)
+            if marker == 1:
+                self.f = f2.f
+        elif kvant_type == "and":
+            marker = 0  # if sets have different sizes, then comparisons will occur on a smaller
+            self.f, marker = fuzzy_kvant.a_and_b(self.f, f2.f)
+            if marker == 1:
+                self.f = f2.f
+        else:
+            raise TypeError
+
+
+def plot_show() -> None:
+    plt.show()
